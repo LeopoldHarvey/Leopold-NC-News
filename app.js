afa7bda7b8abbcf912e88d8532cfefc3978b694f
LeopoldHarvey <LeopoldHarvey42@gmail.com>
2023-08-17 15:29:30 +0100

    ticket 9

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const { handleErrorBadUrl,
 } = require("./controllers/error.controller");
 const {getArticleById, getArticles, getArticleComments, postCommentbyArticleId, patchArticleById,} = require("./controllers/articles.controller");
 const { getEndpoints } = require("./controllers/endpoint.controller")
+const {
+  getCommentById,
+  deleteCommentById,
+} = require("./controllers/comments.controller")
 
 const app = express();
 app.use(express.json());
@@ -17,8 +21,11 @@ app.get('/api/articles',getArticles)
 app.get('/api/articles/:article_id/comments', getArticleComments)
 app.post("/api/articles/:article_id/comments", postCommentbyArticleId);
 app.patch("/api/articles/:article_id", patchArticleById);
+app.get("/api/comments/:comment_id", getCommentById);
+app.delete("/api/comments/:comment_id", deleteCommentById);
 app.use(handleSequlErrors);
 app.use(handleCustomErrors);
+
 app.use((request, response) => {
   response.status(404).send({ msg: "Invalid url" });
 });
