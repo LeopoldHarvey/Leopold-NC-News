823e06cb6dc86ba2ccd2c5318c7dc8da00844c05
LeopoldHarvey <LeopoldHarvey42@gmail.com>
2023-08-18 10:03:59 +0100

    added catch block

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,5 +3,8 @@ const { selectUsers } = require("../models/user.model");
 exports.getUsers = (request, response, next) => {
   selectUsers().then((users) => {
     response.status(200).send({ users });
-  });
+  })
+   .catch((error) => {
+        next(error);
+      });
 };
